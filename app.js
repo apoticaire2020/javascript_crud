4312718c3ede5909587d9412a926d258a0498037
apoticaire2020 <andalossi2008@gmail.com>
2023-06-03 11:57:14 +0100

    using mongodbase

--- app.js
+++ app.js
@@ -8,32 +8,47 @@ async function main(){
     await client.connect();
     console.log('Connected ok' );
    
-    const db = client.db("myTask")
-    const collection = db.collection("documents");
+    const db = client.db("newDb")
+    const docs = db.collection("docs");
    // INSERT 
-//    try {
-//         const insertData = await collection.insertMany([{
-//             name : "ali", age : 30 , sexe : "male" , hobby : "coding"
-//         }, {
-//             name : "mohamed", age : 24 , sexe : "male" , hobby : "lecture"
-//         } , {
-//             name : "aicha", age : 25 , sexe : "female" , hobby : "tennis"
-//         }]);
-//         console.log(" Inserted data => " , insertData );
-//    } catch (e) { throw e}
+   // try {
+   //      const insertData = await docs.insertMany([{
+   //          name : "abbass", age : 30 , sexe : "male" , hobby : "coding"
+   //      }, {
+   //          name : "aissa", age : 31 , sexe : "male" , hobby : "lecture"
+   //      } , {
+   //          name : "khadija", age : 32 , sexe : "female" , hobby : "tennis"
+   //      }]);
+   //      console.log(" Inserted data => " , insertData );
+   // } catch (e) { throw e}
     
 // READ
 
- try {
-     const res =  await collection.find({ sexe : "male"})
-       console.log("doc trouve " , res);
- } catch (e) {
-       throw    e 
- }
-    return 'done !';
-   
+//   try {
+//       const res =  await collection.find({ age : 30})
+//       console.log(await res.toArray());
+//  } catch (e) {throw    e}
+ //UPDATE one
+//  try {
+//     const updateOmar =  await collection.updateOne({ name : "omar"}
+//     ,{$set:{age :65}})
+//     console.log(await updateOmar);
+// } catch (e) {throw    e}
+    
+   // update many 
+//    const res =  await collection.updateMany(
+//     { sexe: 'female' },
+//     { $set: { 'hobby' : "lecture" } }
+//   );
+//  delete 
+//  try {
+//      const deleteemany = await milaffat.deleteMany({});
+//      console.log(await deleteemany);
+//  } catch (e) { throw e}
 
-}
+
+    return 'done !';
+ }
 
 main()
    .then(console.log)
